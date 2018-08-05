dbc500b3d19ac3c4b36f3b4fea8e51d86da84c8c
Ítalo Ferreira Sjobom Nápoles <sjobom.italo@gmail.com>
2018-08-05 16:58:21 -0300

    Inserindo o componente de InputText

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+
+import TodoForm from './components/TodoForm';
 
 export default class TodoApp extends React.Component {
     render() {
         return (
-            <View>
-                <Text>Olá!</Text>
+            <View style={styles.container}>
+                <TodoForm />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 30
+    }
+});
