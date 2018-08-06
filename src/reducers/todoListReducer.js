3a7f38d7032a6c08a597644a2b78f85988d86ffd
Ítalo Ferreira Sjobom Nápoles <sjobom.italo@gmail.com>
2018-08-05 22:14:13 -0300

    Conectando o formulário e adicionando IDs aos Todos

--- src/reducers/todoListReducer.js
+++ src/reducers/todoListReducer.js
@@ -1,12 +1,16 @@
 import { ADD_TODO } from '../actions';
 
+let nextId = 1;
+
 const todoListReducer = (state = [], action) => {
     switch(action.type) {
         case ADD_TODO:
-            //adicionar um todo
+            //adicionar um todo          
             const newTodo = {
+                id: nextId++,
                 text: action.text
             }
+            console.log(newTodo)
             return [...state, newTodo]
         default:
             return state;    
