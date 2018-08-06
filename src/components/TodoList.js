281e3e0b00a67e0c87a83141e1f33c262b6278d1
Ítalo Ferreira Sjobom Nápoles <sjobom.italo@gmail.com>
2018-08-05 22:33:19 -0300

    Listando as todos adicionadas

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
+import { connect } from 'react-redux';
+
 const TodoList = ({ todos = [] }) => (
     <View>
         {todos.map(todo => <Text key={todo.id}>{todo.text}</Text>)}
@@ -12,4 +14,9 @@ const styles = StyleSheet.create({
 
 })
 
-export default TodoList;
+const mapsStateToProps = state => {
+    const { todos } = state;
+    return { todos };
+}
+
+export default connect(mapsStateToProps)(TodoList);
