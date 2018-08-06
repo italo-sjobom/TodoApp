d4f8edb6a1ca89d833ffd0b1b1581443baef15cc
Ítalo Ferreira Sjobom Nápoles <sjobom.italo@gmail.com>
2018-08-05 22:22:48 -0300

    Criação do componente TodoList

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -4,6 +4,8 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
 import rootReducer from './reducers';
 
 const store = createStore(rootReducer); 
@@ -14,6 +16,7 @@ export default class TodoApp extends React.Component {
             <Provider store={store}>
                 <View style={styles.container}>
                     <TodoForm />
+                    <TodoList />
                 </View>
             </Provider>    
         );
